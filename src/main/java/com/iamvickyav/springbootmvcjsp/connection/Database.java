ec901fd62cfff6d3445bff95e6d0e0cc1c11bacb
Mingzhi Zhong <andrewzhong1998@outlook.com>
2019-11-22 01:46:39 -0500

    commit

--- src/main/java/com/iamvickyav/springbootmvcjsp/connection/Database.java
+++ src/main/java/com/iamvickyav/springbootmvcjsp/connection/Database.java
@@ -9,8 +9,8 @@ public class Database {
         try{
             Class.forName("com.mysql.jdbc.Driver");
             String url="jdbc:mysql://localhost:3306/workout_king";
-            String username="root";
-            String password="1998TONYzhong!";
+            String username="workout_king";
+            String password="comp426";
             conn = DriverManager.getConnection(url,username,password);
         }
         catch(Exception e){
